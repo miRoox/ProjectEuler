8faf0ff8d78863074664b66c34b2bcfc6c95933e
miroox <miroox@outlook.com>
2019-12-30 12:18:02 +0800

    add julia test

---
+++ test/runtests.jl
@@ -0,0 +1,31 @@
+using Test
+using ProjectEuler
+
+@testset "ProjectEuler" begin
+    @test pe1()==233168
+    @test pe2()==4613732
+    @test pe3()==6857
+    @test pe4()==906609
+    @test pe5()==232792560
+    @test pe6()==25164150
+    @test pe7()==104743
+    @test pe8()==23514624000
+    @test pe9()==31875000
+    @test pe10()==142913828922
+    @test pe11()==70600674
+    @test pe12()==76576500
+    @test pe13()==5537376230
+    @test pe14()==837799
+    @test pe15()==137846528820
+    @test pe16()==1366
+    @test pe17()==21124
+    @test pe18()==1074
+
+    @test pe20()==648
+
+    @test pe22()==871198282
+
+    @test pe25()==4782
+
+    @test pe34()==40730
+end
